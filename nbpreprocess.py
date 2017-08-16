3e1237ae0b1897c47afcdf3cee8b0b020bf3d962
Thomas Graf <git@thomasgraf.net>
2017-08-15 22:52:27 -0400

    add docstrings, fix some indents

--- nbpreprocess.py
+++ nbpreprocess.py
@@ -13,9 +13,9 @@ build = Path("build")
 notebooks = Path("notebooks")
 templates = Path("templates")
 
-# generator for all standalone files
-tex_exts = (".tikz", ".forest")
-tex = (f for f in source.glob('**/*') if f.suffix in tex_exts)
+# generator for all standalone tikz files
+tikz_exts = (".tikz", ".forest")
+tikz = (f for f in source.glob('**/*') if f.suffix in tikz_exts)
 
 # generator for all mdown files
 mdown_exts = (".mdown", ".md")
@@ -28,16 +28,39 @@ styles = [f for f in templates.glob('**/*') if f.suffix in style_exts]
 
 def change_subfolder(path: Path, subfolder: Path,
                      with_file: bool=False) -> Path:
+    """
+    Prepend path with a subfolder.
+
+    This is a helper function for replicating the path to some file F inside
+    some subfolder A in another subfolder B. It takes the path to F, excluding
+    A, and prepends B. F is also omitted by default.
+
+    Parameters
+    ----------
+    path: Path
+        path to be recreated
+    subfolder: Path
+        folder under which the path is to be created
+    with_file: bool
+        whether the output path should include the file
+    """
     return subfolder / Path(*path.parts[1:]) if with_file\
-      else subfolder / Path(*path.parts[1:-1])
+        else subfolder / Path(*path.parts[1:-1])
 
 
 def mirror_subfolders(path: Path, subfolder: Path) -> None:
+    """Replicate a folder hiearchy inside another folder."""
     change_subfolder(path, subfolder).mkdir(
-            mode=0o755, parents=True, exist_ok=True)
+        mode=0o755, parents=True, exist_ok=True)
 
 
-def process_texfile(f: Path) -> None:
+def process_tikzfile(f: Path) -> None:
+    """
+    Convert tikz file to svg image.
+
+    This requires latexmk and pdf2svg to be installed and available in the
+    user's PATH.
+    """
     for folder in [build, notebooks]:
         mirror_subfolders(f, folder)
 
@@ -55,6 +78,7 @@ def process_texfile(f: Path) -> None:
 
 
 def load_template(file_list: List[str], folder: Path=templates) -> str:
+    """Read in boilerplate text from mdown file."""
     text = "\n"
     for f in file_list:
         path = folder / Path(f + '.mdown')
@@ -65,6 +89,7 @@ def load_template(file_list: List[str], folder: Path=templates) -> str:
 
 
 def regexes(line: str) -> str:
+    """Replace Latex code for processing with notedown."""
     # replace input tikz/forest by image link to svg
     line = re.sub(r"\\input{([^}]*).(tikz|forest)}",
                   r"![alt text](\1.svg)", line)
@@ -77,6 +102,27 @@ def regexes(line: str) -> str:
 def process_mdfile(f: Path,
                    header: List[str]=[],
                    footer: List[str]=[]) -> None:
+    """
+    Convert markdown file to Jupyter notebook.
+
+    This requires notedown to be installed and available in the user's PATH.
+    The markdown file is concatenated with specified header and footer files,
+    preprocessed by regexes, and then fed into notedown.
+
+    The notebook is created in the folder indicated by the notebooks variable,
+    replicating the same folder hierarchy as in the source directory: if
+    markdown file F is found at source/A/B/C/F, then the corresponding notebook
+    file N is at notebook/A/B/C/N.
+
+    Parameters
+    ----------
+    f: Path
+        path to file
+    header:
+        list of header templates to prepend
+    footer:
+        list of footer templates to append
+    """
     with f.open("r") as text:
         for folder in [build, notebooks]:
             mirror_subfolders(f, folder)
@@ -108,8 +154,9 @@ def process_mdfile(f: Path,
             shutil.copy2(str(style), str(target.parent))
 
 
-for f in tex:
-    process_texfile(f)
+# time for the actual processing
+for f in tikz:
+    process_tikzfile(f)
 
 for f in mdown:
     process_mdfile(f, header=['loadcss'])
